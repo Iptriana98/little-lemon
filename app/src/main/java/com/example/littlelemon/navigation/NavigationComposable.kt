cdd368b722799bea871d8e39a06d648bb7081195
ibrahinperez <iptrianaa@gmail.com>
2024-04-09 15:34:32 -0500

    -setup navigation

---
+++ app/src/main/java/com/example/littlelemon/navigation/NavigationComposable.kt
@@ -0,0 +1,34 @@
+package com.example.littlelemon.navigation
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.littlelemon.ui.screens.Home
+import com.example.littlelemon.ui.screens.onboarding.Onboarding
+import com.example.littlelemon.ui.screens.profile.Profile
+
+
+@Composable
+fun NavigationComposable(context: Context, navController: NavHostController) {
+
+    val sharedPreferences = context.getSharedPreferences("Little Lemon", Context.MODE_PRIVATE)
+    var startDestination = Onboarding.route
+
+    if (sharedPreferences.getBoolean("userRegistered", false)) {
+        startDestination = Home.route
+    }
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable(Onboarding.route) {
+            Onboarding(context, navController)
+        }
+        composable(Home.route) {
+            Home(navController)
+        }
+        composable(Profile.route) {
+            Profile(context, navController)
+        }
+    }
+}
